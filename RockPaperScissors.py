7fa81454c58eb052c8be34baa75b08e1c32b57bb
AtanasY <olafdavinci@gmail.com>
2023-02-01 16:22:31 +0200

    Rewrote the entire thing.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,51 +1,29 @@
 import random
+options = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard']
 
-rock = 'Rock'
-paper = 'Paper'
-scissors = 'Scissors'
-spock = 'Spock'
-lizard = 'Lizard'
+
+def play_game(p1, p2):
+    beats = {'Rock': ['Scissors', 'Lizard'], 'Paper': ['Rock', 'Spock'], 'Scissors': ['Paper', 'Lizard'],
+             'Spock': ['Scissors', 'Rock'], 'Lizard': ['Spock', 'Paper']}
+    if p1 == p2:
+        print("It's a tie!")
+    elif p2 in beats[p1]:
+        print("Player wins!")
+    else:
+        print("Computer wins!")
+
 
 while True:
-    player_move = input("Choose [R] - Rock, [P] - Paper, [S] - Scissors, [SP] - Spock or [L] - Lizard: ")
-    if player_move == "R":
-        player_move = rock
-    elif player_move == "P":
-        player_move = paper
-    elif player_move == "S":
-        player_move = scissors
-    elif player_move == "SP":
-        player_move = spock
-    elif player_move == "L":
-        player_move = lizard
-    else:
-        raise SystemExit("Input not yet added. Try again...")
-    computer_random_number = random.randint(1, 5)
-    computer_move = ""
-    if computer_random_number == 1:
-        computer_move = rock
-    if computer_random_number == 2:
-        computer_move = paper
-    if computer_random_number == 3:
-        computer_move = scissors
-    if computer_random_number == 4:
-        computer_move = spock
-    if computer_random_number == 5:
-        computer_move = lizard
-    print(f'The computer chose {computer_move}.')
-    if (player_move == rock and (computer_move == scissors or computer_move == lizard)) or \
-            (player_move == paper and (computer_move == rock or computer_move == spock)) or \
-            (player_move == scissors and (computer_move == paper or computer_move == lizard)) or \
-            (player_move == spock and (computer_move == rock or computer_move == scissors)) or \
-            (player_move == lizard and (computer_move == paper or computer_move == spock)):
-        print("You win!")
-    elif player_move == computer_move:
-        print("Draw!")
-    else:
-        print("You lose!")
-    restart = input("Would you like to play again? [Y] - Yes or [N] - No: ")
-    if restart == "Y":
+    print("Welcome!")
+    Player = input("Rock, Paper, Scissors, Spock, Lizard: ")
+    if Player not in options:
+        print("Invalid Input... Try Again!")
         continue
-    else:
-        print("Thanks for playing!")
+    print(f'Player Chose: {Player}')
+    Computer = random.choice(['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'])
+    print(f"Computer chose: {Computer}")
+    play_game(Player, Computer)
+    restart = input("Play Again? (Y/N): ")
+    if restart.lower() == 'n':
         break
+print("Thanks for playing!")
